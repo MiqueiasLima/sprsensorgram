4e20fbaf96ef436393c8a18dc42b4325f49ba574
MiqueiasLima <miqueiaslima@outlook.com>
2025-08-21 10:14:27 -0300

    Add requirements

---
+++ versions.py
@@ -0,0 +1,16 @@
+import joblib
+import matplotlib
+import scipy
+import streamlit
+import numpy as np
+import tensorflow as tf
+import sklearn as sk
+
+
+print("streamlit:", streamlit.__version__)
+print("numpy:", np.__version__)
+print("matplotlib:", matplotlib.__version__)
+print("tensorflow:", tf.__version__)
+print("joblib:", joblib.__version__)
+print("scikit-learn:", sk.__version__)
+print("scipy:", scipy.__version__)
