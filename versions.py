3bbde698b82256a45dc54c478a4ace64ab24f0ab
MiqueiasLima <miqueiaslima@outlook.com>
2025-08-21 10:42:28 -0300

    add pandas

--- versions.py
+++ versions.py
@@ -1,5 +1,5 @@
 import joblib
-import matplotlib
+import matplotlib.pyplot as mp
 import scipy
 import streamlit
 import numpy as np
@@ -9,7 +9,7 @@ import sklearn as sk
 
 print("streamlit:", streamlit.__version__)
 print("numpy:", np.__version__)
-print("matplotlib:", matplotlib.__version__)
+print("matplotlib: ",mp)
 print("tensorflow:", tf.__version__)
 print("joblib:", joblib.__version__)
 print("scikit-learn:", sk.__version__)
